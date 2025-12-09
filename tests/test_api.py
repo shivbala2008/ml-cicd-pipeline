fc2e5b67feed61d9925321de89c7d040e009c4ce
Balakrishnan <sbalak015@apac.comcast.com>
2025-12-09 15:44:26 +0530

    fixed issue with test_apy.py file

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,7 +7,7 @@ import pytest
 # Add src to path
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 
-from predict import ModelPredictor, app
+from predict import ModelPredictor, APP
 
 
 class TestPredictionAPI:
